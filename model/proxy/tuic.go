2c536bbe4d81f81d93d226dfafc0bc2ec1c872f6
nite <admin@nite07.com>
2026-08-06 16:28:17 +0800

    fix: add yaml tags to tuic model to omit zero values

--- model/proxy/tuic.go
+++ model/proxy/tuic.go
@@ -1,35 +1,36 @@
 package proxy
 
+// https://github.com/MetaCubeX/mihomo/blob/Meta/adapter/outbound/tuic.go
 type Tuic struct {
-	Server                string   `proxy:"server"`
-	Port                  int      `proxy:"port"`
-	Token                 string   `proxy:"token,omitempty"`
-	UUID                  string   `proxy:"uuid,omitempty"`
-	Password              string   `proxy:"password,omitempty"`
-	Ip                    string   `proxy:"ip,omitempty"`
-	HeartbeatInterval     int      `proxy:"heartbeat-interval,omitempty"`
-	ALPN                  []string `proxy:"alpn,omitempty"`
-	ReduceRtt             bool     `proxy:"reduce-rtt,omitempty"`
-	RequestTimeout        int      `proxy:"request-timeout,omitempty"`
-	UdpRelayMode          string   `proxy:"udp-relay-mode,omitempty"`
-	CongestionController  string   `proxy:"congestion-controller,omitempty"`
-	DisableSni            bool     `proxy:"disable-sni,omitempty"`
-	MaxUdpRelayPacketSize int      `proxy:"max-udp-relay-packet-size,omitempty"`
+	Server                string   `yaml:"server" proxy:"server"`
+	Port                  int      `yaml:"port" proxy:"port"`
+	Token                 string   `yaml:"token,omitempty" proxy:"token,omitempty"`
+	UUID                  string   `yaml:"uuid,omitempty" proxy:"uuid,omitempty"`
+	Password              string   `yaml:"password,omitempty" proxy:"password,omitempty"`
+	Ip                    string   `yaml:"ip,omitempty" proxy:"ip,omitempty"`
+	HeartbeatInterval     int      `yaml:"heartbeat-interval,omitempty" proxy:"heartbeat-interval,omitempty"`
+	ALPN                  []string `yaml:"alpn,omitempty" proxy:"alpn,omitempty"`
+	ReduceRtt             bool     `yaml:"reduce-rtt,omitempty" proxy:"reduce-rtt,omitempty"`
+	RequestTimeout        int      `yaml:"request-timeout,omitempty" proxy:"request-timeout,omitempty"`
+	UdpRelayMode          string   `yaml:"udp-relay-mode,omitempty" proxy:"udp-relay-mode,omitempty"`
+	CongestionController  string   `yaml:"congestion-controller,omitempty" proxy:"congestion-controller,omitempty"`
+	DisableSni            bool     `yaml:"disable-sni,omitempty" proxy:"disable-sni,omitempty"`
+	MaxUdpRelayPacketSize int      `yaml:"max-udp-relay-packet-size,omitempty" proxy:"max-udp-relay-packet-size,omitempty"`
 
-	FastOpen             bool       `proxy:"fast-open,omitempty"`
-	MaxOpenStreams       int        `proxy:"max-open-streams,omitempty"`
-	CWND                 int        `proxy:"cwnd,omitempty"`
-	SkipCertVerify       bool       `proxy:"skip-cert-verify,omitempty"`
-	Fingerprint          string     `proxy:"fingerprint,omitempty"`
-	Certificate          string     `proxy:"certificate,omitempty"`
-	PrivateKey           string     `proxy:"private-key,omitempty"`
-	ReceiveWindowConn    int        `proxy:"recv-window-conn,omitempty"`
-	ReceiveWindow        int        `proxy:"recv-window,omitempty"`
-	DisableMTUDiscovery  bool       `proxy:"disable-mtu-discovery,omitempty"`
-	MaxDatagramFrameSize int        `proxy:"max-datagram-frame-size,omitempty"`
-	SNI                  string     `proxy:"sni,omitempty"`
-	ECHOpts              ECHOptions `proxy:"ech-opts,omitempty"`
+	FastOpen             bool       `yaml:"fast-open,omitempty" proxy:"fast-open,omitempty"`
+	MaxOpenStreams       int        `yaml:"max-open-streams,omitempty" proxy:"max-open-streams,omitempty"`
+	CWND                 int        `yaml:"cwnd,omitempty" proxy:"cwnd,omitempty"`
+	SkipCertVerify       bool       `yaml:"skip-cert-verify,omitempty" proxy:"skip-cert-verify,omitempty"`
+	Fingerprint          string     `yaml:"fingerprint,omitempty" proxy:"fingerprint,omitempty"`
+	Certificate          string     `yaml:"certificate,omitempty" proxy:"certificate,omitempty"`
+	PrivateKey           string     `yaml:"private-key,omitempty" proxy:"private-key,omitempty"`
+	ReceiveWindowConn    int        `yaml:"recv-window-conn,omitempty" proxy:"recv-window-conn,omitempty"`
+	ReceiveWindow        int        `yaml:"recv-window,omitempty" proxy:"recv-window,omitempty"`
+	DisableMTUDiscovery  bool       `yaml:"disable-mtu-discovery,omitempty" proxy:"disable-mtu-discovery,omitempty"`
+	MaxDatagramFrameSize int        `yaml:"max-datagram-frame-size,omitempty" proxy:"max-datagram-frame-size,omitempty"`
+	SNI                  string     `yaml:"sni,omitempty" proxy:"sni,omitempty"`
+	ECHOpts              ECHOptions `yaml:"ech-opts,omitempty" proxy:"ech-opts,omitempty"`
 
-	UDPOverStream        bool `proxy:"udp-over-stream,omitempty"`
-	UDPOverStreamVersion int  `proxy:"udp-over-stream-version,omitempty"`
+	UDPOverStream        bool `yaml:"udp-over-stream,omitempty" proxy:"udp-over-stream,omitempty"`
+	UDPOverStreamVersion int  `yaml:"udp-over-stream-version,omitempty" proxy:"udp-over-stream-version,omitempty"`
 }
